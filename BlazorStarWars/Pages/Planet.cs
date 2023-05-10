e24c6e3157fef41cb1fce6cc674c59e076328386
Endri Dibra <104068619+EndriDibra@users.noreply.github.com>
2023-05-11 02:08:10 +0300

    Update and rename Film.cs to Planet.cs

---
+++ BlazorStarWars/Pages/Planet.cs
@@ -0,0 +1,9 @@
+namespace BlazorStarWars.Pages
+{
+	public class Planet
+	{
+		public string Name { get; set; }
+		public int Rotation_Period { get; set; }
+		public int Orbital_Period { get; set; }
+	}
+}
