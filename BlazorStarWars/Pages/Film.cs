56edfa100b8ff67fa2ecb8cd8034abd46f9f88c4
Endri Dibra <104068619+EndriDibra@users.noreply.github.com>
2023-05-10 05:06:21 +0300

    Add files via upload

---
+++ BlazorStarWars/Pages/Film.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorStarWars.Pages
+{
+	public class Film
+	{
+		public string Title { get; set; }
+		public int EpisodeId { get; set; }
+		public string Director { get; set; }
+		public string Producer { get; set; }
+		public DateTime ReleaseDate { get; set; }
+	}
+}
