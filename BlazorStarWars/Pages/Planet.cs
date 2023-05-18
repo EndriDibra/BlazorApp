736a83266729e9dd7d4adc4238e15296e6503af4
Endri Dibra <104068619+EndriDibra@users.noreply.github.com>
2023-05-18 16:29:01 +0300

    Update Planet.cs

--- BlazorStarWars/Pages/Planet.cs
+++ BlazorStarWars/Pages/Planet.cs
@@ -2,8 +2,8 @@ namespace BlazorStarWars.Pages
 {
 	public class Planet
 	{
-		public string Name { get; set; }
-		public int Rotation_Period { get; set; }
-		public int Orbital_Period { get; set; }
+		public string Name { get; set; } = "";
+		public int Rotation_Period { get; set; } = 0;
+		public int Orbital_Period { get; set; } = 0;
 	}
 }
