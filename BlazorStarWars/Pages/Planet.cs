3e08529f42493c83be952e440e8c79177a23253b
Endri Dibra <104068619+EndriDibra@users.noreply.github.com>
2023-06-17 18:11:57 +0300

    Delete Planet.cs

--- BlazorStarWars/Pages/Planet.cs
+++
@@ -1,9 +0,0 @@
-namespace BlazorStarWars.Pages
-{
-	public class Planet
-	{
-		public string Name { get; set; } = "";
-		public int Rotation_Period { get; set; } = 0;
-		public int Orbital_Period { get; set; } = 0;
-	}
-}
